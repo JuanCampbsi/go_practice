1f72e4f4da60b7f1e00d80bc389344387461452e
Juan.Ferreira <juan.ferreira@impar.com.br>
2023-05-15 20:12:22 -0300

    first commit

---
+++ hello.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"time"
+)
+
+const monitoring = 3
+const delay = 5
+
+func main() {
+	viewIntrod()
+	for {
+		viewMenu()
+		comand := readComand()
+
+		switch comand {
+		case 1:
+			initialMonitoring()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
+	}
+}
+
+func initialMonitoring() {
+	conditionClearConsole()
+	fmt.Println("Monitorando....")
+	site := readSitesFiles()
+	isVerifiyMonitoring := false
+
+	for i := 0; i < monitoring; i++ {
+		if isVerifiyMonitoring {
+			fmt.Println("Monitorando....")
+		}
+		for _, url := range site {
+			fmt.Println("\nTestando site...")
+			testSite(url)
+		}
+		isVerifiyMonitoring = true
+		time.Sleep(delay * time.Second)
+		conditionClearConsole()
+	}
+
+	fmt.Println("\nTamanho do array", len(site))
+	fmt.Println("Capacidade do array", cap(site))
+
+	site = append(site, "https://www.google.com/")
+}
+
+func viewIntrod() {
+	name := "Juan"
+	version := 1.1
+	age := 28
+
+	fmt.Printf("\nHello Mr. %s. This age %d \n", name, age)
+	fmt.Println("This is system of version", version)
+}
+
+func viewMenu() {
+	fmt.Println("\n1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir logs")
+	fmt.Println("0- Sair do programa")
+}
+
+func readComand() int {
+	var comand int
+	fmt.Scan(&comand)
+	fmt.Println("\nO comando escolhido foi", comand)
+
+	return comand
+}
+
+func testSite(site string) {
+	resp, err := http.Get(site)
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso! Status code:", resp.StatusCode)
+	} else {
+		fmt.Println("Site:", site, "esta com problemas. Status code:", resp.StatusCode)
+	}
+}
+
+func conditionClearConsole() {
+	clearCmd := ""
+	if runtime.GOOS == "windows" {
+		clearCmd = "cls"
+	} else {
+		clearCmd = "clear"
+	}
+	cmd := exec.Command(clearCmd)
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func readSitesFiles() []string {
+	var sites []string
+	files, err := os.Open("helloApp/sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+	read := bufio.NewReader(files)
+	for {
+		line, err := read.ReadString('\n')
+		line = strings.TrimSpace(line)
+		sites = append(sites, line)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	files.Close()
+
+	return sites
+}
